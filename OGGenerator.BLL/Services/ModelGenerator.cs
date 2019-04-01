7bcfe314df7240a8a471351de9cd4f1f8b4a4f4b
Andrew Moraes <34313674+anddMF@users.noreply.github.com>
2019-04-01 10:22:05 -0300

    Initial commit

---
+++ OGGenerator.BLL/Services/ModelGenerator.cs
@@ -0,0 +1,32 @@
+﻿using OGGenerator.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OGGenerator.BLL.Services
+{
+    public class ModelGenerator : IModelGenerator
+    {
+        public void DoGenerator(string table, CommandType type)
+        {
+            var conn = new OGGenerator.Infrastructure.ConnectionFactory();
+
+            SqlDataReader result = conn.GetReader("select top 1 * from " + table, CommandType.Text);
+
+            DataTable dt = new DataTable();
+            dt.Load(result);
+
+            foreach (DataColumn column in dt.Columns)
+            {
+                var t = column.DataType.Name;
+                var c = column.ColumnName;
+                Console.WriteLine("public "+ t + " " + c + " { get; set; } ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OGGenerator.Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using OGGenerator.BLL.Services;
+using OGGenerator.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OGGenerator.ConsoleOps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region compile
+            Console.WriteLine("Insira a table: ");
+            var tableName = Console.ReadLine();
+            
+            IModelGenerator svc = new ModelGenerator();
+            svc.DoGenerator(tableName, CommandType.Text);
+
+            Console.ReadKey();
+            #endregion
+        }
+    }
+}
